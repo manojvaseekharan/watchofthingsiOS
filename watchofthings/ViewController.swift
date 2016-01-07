383ba2c0d3398c71a4655273165fb55311a5031e
Manoj Vaseekharan <manojvaseekharan@manojs-mac.local>
2016-01-07 16:30:11 +0000

    Initial Commit

---
+++ watchofthings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  watchofthings
+//
+//  Created by Manoj Vaseekharan on 07/01/2016.
+//  Copyright © 2016 Manoj Vaseekharan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
